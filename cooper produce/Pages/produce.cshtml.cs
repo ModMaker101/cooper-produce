49988985dff55582370ad43d9e2778fcf4c29ffd
PRIMandSLIM <123708239+chasecod3r@users.noreply.github.com>
2023-05-19 16:01:32 -0400

    New stuff

---
+++ cooper produce/Pages/produce.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cooper_produce.Pages
+{
+    public class produceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
